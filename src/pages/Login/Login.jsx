81a320f4d538be5987b4f32ec00965013a84ff99
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-03 17:28:50 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import LoginButton from '../../components/Login/LoginButton';
+
+const SOCIAL_LOGIN_LIST = ['kakao'];
 
 const Login = () => {
-  return <div>로그인</div>;
+  const renderLogo = () => {
+    // TODO: 서비스 로고 추가 예정
+    return <LogoContainer>ODIT</LogoContainer>;
+  };
+
+  const renderLoginButtons = () => {
+    return (
+      <ButtonContatiner>
+        {SOCIAL_LOGIN_LIST.map((social) => (
+          <LoginButton social={social} />
+        ))}
+      </ButtonContatiner>
+    );
+  };
+
+  return (
+    <Wrapper>
+      {renderLogo()}
+      {renderLoginButtons()}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const ButtonContatiner = styled.div`
+  display: flex;
+  align-items: end;
+`;
 
 export default Login;
