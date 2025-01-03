a3c1ec69cf4480bc19800a449408cde4e0c743a4
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-03 17:28:40 +0900

    feat: 로그인 버튼 컴포넌트 구현

---
+++ src/components/Login/LoginButton.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { ReactComponent as KakaoIcon } from '../../assets/icons/kakao-icon.svg';
+
+const LoginButton = ({ social }) => {
+  const getButtonStyles = () => {
+    if (social === 'kakao') {
+      return {
+        background: '#F9E007',
+        text: '카카오로 로그인하기',
+        icon: <StyledKakaoIcon />,
+      };
+    }
+
+    return {
+      background: '#FFFFFF',
+      icon: null,
+    };
+  };
+
+  const { background, text, icon } = getButtonStyles();
+
+  return (
+    <StyledButton $background={background}>
+      {icon}
+      {text}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  position: relative;
+  width: 20rem;
+  height: 3.75rem;
+  background: ${({ $background }) => $background};
+  border-radius: 0.625rem;
+  border: none;
+  font-size: 1rem;
+  font-weight: 700;
+
+  > svg {
+    position: absolute;
+    top: 50%;
+    left: 1.5rem;
+    transform: translateY(-50%);
+  }
+`;
+
+const StyledKakaoIcon = styled(KakaoIcon)`
+  width: 1.5rem;
+  fill: #3e2723;
+`;
+
+export default LoginButton;
