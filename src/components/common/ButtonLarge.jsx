698845495f1bcc54fc1c701b343e1980dedd30e2
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-20 23:42:51 +0900

    feat: implement ButtonLarge component

---
+++ src/components/common/ButtonLarge.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { styled, Button } from '@mui/material';
+import { useTheme } from '@emotion/react';
+
+const StyledButtonLarge = styled(Button)(({ style }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '21rem',
+  height: '3.5rem',
+  borderRadius: '0.75rem',
+  border: `1px solid ${style.borderColor}`,
+  backgroundColor: style.backgroundColor,
+  color: style.color,
+  fontSize: '1rem',
+  fontWeight: 700,
+}));
+
+/**
+ * ButtonLarge component
+ *
+ * @param {React.ReactNode} children - The content of the button.
+ * @param {string} color - The color style of the button. Default is an empty string.
+ * @param {function} onClick - Function to call when the button is clicked. Default is an empty function.
+ */
+const ButtonLarge = ({ children, color = '', onClick = () => {} }) => {
+  const theme = useTheme();
+
+  const getButtonStyle = () => {
+    switch (color) {
+      case 'enabled':
+        return {
+          backgroundColor: theme.color.main[100],
+          color: theme.color.black[0],
+          borderColor: theme.color.main[100],
+        };
+      case 'disabled':
+        return {
+          backgroundColor: theme.color.black[100],
+          color: theme.color.black[400],
+          borderColor: theme.color.black[100],
+        };
+      default:
+        return {
+          backgroundColor: theme.color.black[0],
+          color: theme.color.black[600],
+          borderColor: theme.color.black[100],
+        };
+    }
+  };
+
+  return (
+    <StyledButtonLarge disabled={color === 'disabled'} style={getButtonStyle()} onClick={onClick}>
+      {children}
+    </StyledButtonLarge>
+  );
+};
+
+export default ButtonLarge;
