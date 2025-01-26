e1f1421d86797d3c4eadefeac6050b241c0431eb
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-26 17:34:06 +0900

    feat: 장소 추가 페이지 경로 설정

--- src/router/addRouter.js
+++ src/router/addRouter.js
@@ -3,6 +3,7 @@ import React from 'react';
 const AsyncAdd = React.lazy(() => import('../pages/Add/Add'));
 const AsyncAddPlaceExpotLink = React.lazy(() => import('../pages/Add/Place/AddPlaceExpotLink'));
 const AsyncAddPlaceLoading = React.lazy(() => import('../pages/Add/Place/AddPlaceLoading'));
+const AsyncAddPlace = React.lazy(() => import('../pages/Add/Place/AddPlace'));
 
 const addRouter = [
   {
@@ -10,12 +11,16 @@ const addRouter = [
     element: <AsyncAdd />,
   },
   {
-    path: 'place',
+    path: 'export',
     element: <AsyncAddPlaceExpotLink />,
   },
   {
     path: 'loading',
     element: <AsyncAddPlaceLoading />,
+  },
+  {
+    path: 'place',
+    element: <AsyncAddPlace />,
   },
 ];
 
