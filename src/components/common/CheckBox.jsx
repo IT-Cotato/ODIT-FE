4fb4c9f2df2f8634b6caf1890c67171f3e90f7b3
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-27 00:22:17 +0900

    feat: 체크박스 구현

---
+++ src/components/common/CheckBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Checkbox } from '@mui/material';
+import { useTheme } from '@emotion/react';
+
+const CheckBox = ({ checked, onChange }) => {
+  const theme = useTheme();
+
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={onChange}
+      sx={{
+        color: theme.color.black[200],
+
+        '& .MuiSvgIcon-root': {
+          width: '2rem',
+          height: '2rem',
+        },
+
+        '&.Mui-checked': {
+          color: theme.color.main[100],
+        },
+      }}
+    />
+  );
+};
+
+export default CheckBox;
