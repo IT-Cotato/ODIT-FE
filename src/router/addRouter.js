28cb393201822a572143da679c0b98e69e4020f9
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-26 17:17:35 +0900

    feat: add router 경로 설정

--- src/router/addRouter.js
+++ src/router/addRouter.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const AsyncAdd = React.lazy(() => import('../pages/Add/Add'));
+const AsyncAddPlaceExpotLink = React.lazy(() => import('../pages/Add/Place/AddPlaceExpotLink'));
+const AsyncAddPlaceLoading = React.lazy(() => import('../pages/Add/Place/AddPlaceLoading'));
 
 const addRouter = [
   {
     path: '',
     element: <AsyncAdd />,
+  },
+  {
+    path: 'place',
+    element: <AsyncAddPlaceExpotLink />,
+  },
+  {
+    path: 'loading',
+    element: <AsyncAddPlaceLoading />,
   },
 ];
 
