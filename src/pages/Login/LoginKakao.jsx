edfb09008a61ac5fd9f4f12840b480ddb6b8a7c8
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-27 18:20:34 +0900

    feat: 인가코드 발행 이후 login 요청

--- src/pages/Login/LoginKakao.jsx
+++ src/pages/Login/LoginKakao.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+import { getLogin } from '../../apis/auth';
+import AuthGuard from '../../components/common/AuthGuard';
 
-const KaKaoAuth = () => {
+const LoginKakao = () => {
+  const navigate = useNavigate();
+
   const authorizedCode = new URLSearchParams(window.location.search).get('code');
 
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+    const login = async () => {
+      try {
+        const data = await getLogin(AuthGuard);
+      } catch {
+        navigate('login', {
+          state: {
+            fail: true,
+          },
+        });
+      }
+    };
+
+    login();
+  }, []);
+
+  if (!authorizedCode) {
+    // navigate('/login', search: );
+  }
 
   return <div>로그인중...</div>;
 };
 
-export default KaKaoAuth;
+export default LoginKakao;
