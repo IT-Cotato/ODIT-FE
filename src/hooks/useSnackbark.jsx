142785b3fd2c0e812b0163e608d9cc73bf49f756
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-27 18:19:55 +0900

    feat: 스낵바 구현

---
+++ src/hooks/useSnackbark.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Snackbar, Alert, Slide } from '@mui/material';
+
+const DEFAULT_DURATION = 1200;
+
+export const useSnackbar = () => {
+  const [snackbar, setSnackbar] = React.useState({
+    open: false,
+    message: '',
+    type: 'error',
+    duration: DEFAULT_DURATION,
+  });
+
+  const showSnackbar = (message, type = 'error', duration = DEFAULT_DURATION) => {
+    setSnackbar({ open: true, message, type, duration });
+    // setTimeout(() => {
+    //   setSnackbar({ open: false, message: '', type: 'error' });
+    // }, duration);
+  };
+
+  const SnackbarComponent = () => (
+    <Snackbar
+      open={snackbar.open}
+      autoHideDuration={snackbar.duration}
+      onClose={() => setSnackbar({ ...snackbar, open: false })}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      // eslint-disable-next-line react/no-unstable-nested-components, react/jsx-props-no-spreading
+      TransitionComponent={(props) => <Slide {...props} direction="up" />}
+    >
+      <Alert severity={snackbar.type} onClose={() => setSnackbar({ ...snackbar, open: false })}>
+        {snackbar.message}
+      </Alert>
+    </Snackbar>
+  );
+
+  return { showSnackbar, SnackbarComponent };
+};
+
+export default useSnackbar;
