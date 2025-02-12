c36e08a45a770b6d73097acd00690324fe8ce88b
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-02-13 08:40:53 +0900

    chore: add category map

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,22 +1,27 @@
 export const HEADER_HEIGHT = '4.5rem';
 
-export const PLACE_CATEGORY = [
-  '대형마트',
-  '편의점',
-  '어린이집, 유치원',
-  '학교',
-  '학원',
-  '주차장',
-  '주유소, 충전소',
-  '지하철역',
-  '은행',
-  '문화시설',
-  '중개업소',
-  '공공기관',
-  '관광명소',
-  '숙박',
-  '음식점',
-  '카페',
-  '병원',
-  '약국',
-];
+export const PLACE_CATEGORY_CODE_MAP = {
+  MT1: '대형마트',
+  CS2: '편의점',
+  PS3: '어린이집, 유치원',
+  SC4: '학교',
+  AC5: '학원',
+  PK6: '주차장',
+  OL7: '주유소, 충전소',
+  SW8: '지하철역',
+  BK9: '은행',
+  CT1: '문화시설',
+  AG2: '중개업소',
+  PO3: '공공기관',
+  AT4: '관광명소',
+  AD5: '숙박',
+  FD6: '음식점',
+  CE7: '카페',
+  HP8: '병원',
+  PM9: '약국',
+};
+
+export const EVENT_CATEGORY_CODE_MAP = {
+  PERFORMANCE: '공연',
+  EXHIBITION: '전시',
+};
