044767653f6cad7d9fd7bb653f0fb7b8199bc8a7
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-02-13 17:11:58 +0900

    feat: implement place search in map list

--- src/pages/Map/MapList.jsx
+++ src/pages/Map/MapList.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Box, Stack } from '@mui/material';
+import { Box, Fade, Stack } from '@mui/material';
 import TextFieldLarge from '../../components/common/TextFieldLarge';
 import usePlaces from '../../hooks/usePlaces';
 import MapListItem from '../../components/Map/MapListItem';
 import useIsBottomDrawerFullOpenStore from '../../stores/useIsBottomDrawerFullOpenStore';
 import MapListCategoryFilter from './MapListCategoryFilter';
 import { PLACE_CATEGORY_CODE_WITH_ALL_MAP } from '../../constant';
+import useDebounce from '../../hooks/useDebounce';
 
 const MapList = () => {
   const [checkedCategories, setCheckedCategories] = React.useState(['ALL']);
+  const [searchText, setSearchText] = React.useState('');
+
+  const debouncedSearchText = useDebounce({ value: searchText, delay: 500 });
 
   const { isBottomDrawerFullOpen } = useIsBottomDrawerFullOpenStore();
 
-  const { places, handleFilterCategories } = usePlaces();
+  const { places, handleFilterCategories, handleSearchKeyword } = usePlaces();
+
+  const handleSearchTextChange = (changedText) => {
+    setSearchText(changedText);
+  };
 
   const handleCheckedCategoriesChange = (code) => {
     if (code === 'ALL' && !checkedCategories.includes(code)) {
@@ -32,6 +40,10 @@ const MapList = () => {
     handleFilterCategories(checkedCategories);
   }, [checkedCategories]);
 
+  React.useEffect(() => {
+    handleSearchKeyword(debouncedSearchText);
+  }, [debouncedSearchText]);
+
   return (
     <Box
       sx={{
@@ -40,6 +52,26 @@ const MapList = () => {
         gap: '1rem',
       }}
     >
+      {isBottomDrawerFullOpen && (
+        <Fade in={isBottomDrawerFullOpen}>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '1.5rem',
+              marginBottom: '-1rem',
+            }}
+          >
+            <TextFieldLarge
+              outlined={false}
+              placeholder="내가 저장한 장소를 검색하세요."
+              value={searchText}
+              onChange={handleSearchTextChange}
+            />
+          </Box>
+        </Fade>
+      )}
       <MapListCategoryFilter checkedCategories={checkedCategories} onChange={handleCheckedCategoriesChange} />
       <Stack sx={{ gap: '1rem', width: '100%', height: '100%', flex: '1', overflow: 'scroll' }}>
         {places
