133bf6542415f5497f5deb228228109891f537e5
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-02-15 17:16:53 +0900

    feat: render places marker by category

--- src/hooks/useKakaomap.jsx
+++ src/hooks/useKakaomap.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { useTheme } from '@emotion/react';
 import useCurrentLocation from './useCurrentLocation';
 import usePlaces from './usePlaces';
+import pinFoodDefault from '../assets/icons/pin_food_default.svg';
+import pinCafeDefault from '../assets/icons/pin_cafe_default.svg';
+import pinHotelDefault from '../assets/icons/pin_hotel_default.svg';
+import pinCultureDefault from '../assets/icons/pin_culture_default.svg';
+import pinOthersDefault from '../assets/icons/pin_others_default.svg';
+
+const CATEGORY_PIN_ICON_MAP = {
+  FD6: pinFoodDefault,
+  CE7: pinCafeDefault,
+  AD5: pinHotelDefault,
+  CT1: pinCultureDefault,
+  AT4: pinCultureDefault,
+};
 
 const useKakaomap = ({ mapContainerRef }) => {
   const mapRef = React.useRef(null);
-
-  const theme = useTheme();
+  const markersRef = React.useRef([]);
 
   const { isLoding: isCurrentLocationLoading, currentLocation } = useCurrentLocation();
 
@@ -17,24 +28,24 @@ const useKakaomap = ({ mapContainerRef }) => {
       return;
     }
 
+    markersRef.current.forEach((marker) => marker.setMap(null));
+    markersRef.current = [];
+
     places.forEach((place) => {
       const markerContainer = document.createElement('div');
 
-      markerContainer.style.width = '1.5rem';
-      markerContainer.style.height = '1.5rem';
-      // eslint-disable-next-line prefer-destructuring
-      markerContainer.style.backgroundColor = theme.color.main[50];
-      markerContainer.style.borderRadius = '50%';
-      markerContainer.style.border = `1px solid ${theme.color.black[0]}`;
+      markerContainer.style.backgroundImage = `url(${CATEGORY_PIN_ICON_MAP[place.subCategory] || pinOthersDefault})`;
+      markerContainer.style.backgroundSize = 'contain';
+      markerContainer.style.width = '2.5rem';
+      markerContainer.style.height = '2.5rem';
 
       const customOverlay = new window.kakao.maps.CustomOverlay({
         position: new window.kakao.maps.LatLng(place.latitude, place.longitude),
         content: markerContainer,
-        yAnchor: 0.5,
-        xAnchor: 0.5,
       });
 
       customOverlay.setMap(mapRef.current);
+      markersRef.current.push(customOverlay);
     });
   }, [isPlacesLoding, places, mapRef.current]);
 
